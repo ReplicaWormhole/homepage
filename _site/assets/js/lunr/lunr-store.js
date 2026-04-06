86b1e7dcb4c01e5b8b5b8a312f282b4e024fdac1
Lev <leoshaposhnik@googlemail.com>
2026-04-06 10:07:25 +0200

    update page design

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "On Science in the age of AI",
+        "excerpt":"We are facing increasingly superhuman abilities of AI systems to perform exploratory tasks in fundamental science. Two days ago Sidharth Hariharan and Maryna Viazovska together with Chris Birkbeck, Seewoo Lee, Gareth Ma and Math. Inc. announced the formalization of the sphere packing problem in dimensions 8 and 24.1 Yesterday OpenAI...","categories": [],
+        "tags": [],
+        "url": "/homepage/2026/03/04/science_ai.html",
+        "teaser": null
+      }]
